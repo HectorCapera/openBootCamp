e1b201273735811598ca4801b5a74fa9ead18331
Hector Capera <hectorcapera415@gmail.com>
2022-12-07 17:36:11 -0500

    JavaScript

---
+++ JavaScript/Ejercicio2.js
@@ -0,0 +1,18 @@
+
+let num = 10
+
+for (let i = num -1; i >= 1; i--){
+    num = num *i;
+    console.log(num)
+}
+
+
+if (num == 10) 
+
+while (num == 10) { 
+    num--;
+    num = num *i;
+    break;
+}
+console.log(num)
+
